2be4fdbcf573487ccae157e1fbdd1f2646533179
samhayder <sams.seul@gmail.com>
2023-02-07 22:49:01 +0600

    Array Continue

--- test.js
+++ test.js
@@ -1,56 +1,11 @@
+const  list = `{
+    "students":[
+        {"Yasin": "One"},
+        {"Jannat": "Two"},
+        {"Samsuddin": "Three"}
+    ]
+}`
 
-/* let userName = '';
-let password;
+const obj = JSON.parse(list);
 
-let checkUserName = userName || "admin";
-let checkPassword = password || 123456;
-
-
-let login = true;
-let loginMsg = login && "Login successful";
-
-console.log(checkUserName)
-console.log(checkPassword)
-console.log(loginMsg)
- */
-
-//while loop
-/* let isPlay = true;
-
-while (isPlay){
-    let win = Math.round(Math.random() * 6);
-
-    if (win === 6){
-        console.warn('You Winner');
-        isPlay = false;
-    }else{
-        console.log('You god ' + win);
-    }
-} */
-
-//Nested loops
-let num = 5;
-
-/* for (let row = 1; row <= num; row++){
-    let result = '';
-    for (let col = 1; col <= row; col++){
-        result += col + ' ';
-    }
-    console.log(result);
-} */
-
-for (let row = 1; row <= num; row++){
-    let result = '';
-    for (let col = 1; col <= num; col++){
-        result +='* ';
-    }
-    console.log(result);
-}
-
-
-
-
-
-
-
-
+console.log(obj);
